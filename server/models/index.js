0e28d7d4eab57934e6d755976bce2b4683ab0430
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-16 12:58:00 +0200

    setup
    include DB config

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.USER,
   host: process.env.HOST,
-  database: 'TradeBattlesBackend',
+  database: process.env.DBNAME,
   password: process.env.PASSWORD,
   port: process.env.PORT,
 });
