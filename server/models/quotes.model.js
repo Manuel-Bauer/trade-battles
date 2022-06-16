bf71fc9ba78865bee59f725ed034b59fc2d69503
Manuel-Bauer <manuel.c.bauer@googlemail.com>
2022-06-16 13:14:17 +0200

    setup
    changed from config to env

--- server/models/quotes.model.js
+++ server/models/quotes.model.js
@@ -1,6 +1,4 @@
 const fetch = require('node-fetch');
-const { IEX_APIKEY } = require('./config');
-const { POLYGON_APIKEY } = require('./config');
 require('dotenv').config();
 
 const IEXapibaseurl = process.env.IEX_API_KEY;
@@ -8,7 +6,7 @@ const PoligonApiBaseUrl = process.env.POLYGON_API_KEY;
 
 exports.getQuote = async (symbol) => {
   const quote = await fetch(
-    `${IEXapibaseurl}/stock/${symbol.toLowerCase()}/quote?token=${IEX_APIKEY}`
+    `${IEXapibaseurl}/stock/${symbol.toLowerCase()}/quote?token=${IEXapibaseurl}`
   ).then((res) => res.json());
 
   console.log(quote);
@@ -30,12 +28,12 @@ exports.getHistoricalData = async (
   };
   const startDate = convertDate(start_date);
   const endDate = convertDate(end_date);
-  const url = `${PoligonApiBaseUrl}/${ticker.toUpperCase()}/range/${periodicity_unit}/${periodicity}/${start_date}/${end_date}?adjusted=true&sort=asc`;
+  const url = `${IEXapibaseurl}/${ticker.toUpperCase()}/range/${periodicity_unit}/${periodicity}/${start_date}/${end_date}?adjusted=true&sort=asc`;
   console.log(url);
   const data = await fetch(url, {
     method: 'GET',
     headers: {
-      Authorization: `Bearer ${process.env.POLYGON_API_KEY}`,
+      Authorization: `Bearer ${PoligonApiBaseUrl}`,
     },
   }).then((res) => res.json());
   console.log(data);
