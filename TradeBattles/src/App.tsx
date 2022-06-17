0b4b5e07790139dc0fe4ae6547527e2d463f8238
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-17 15:59:39 +0200

    added background imgaes

--- TradeBattles/src/App.tsx
+++ TradeBattles/src/App.tsx
@@ -2,17 +2,26 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './screens/LoginStackNavigator.navigation';
 import {UserProvider, ThemeProvider} from './App.provider';
-import {LogBox} from 'react-native';
+import {ImageBackground, LogBox} from 'react-native';
 LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
 LogBox.ignoreAllLogs(); //Ignore all log notifications
 import {showNotification} from './shared/Notification';
-
+import {SafeAreaView} from 'react-native-safe-area-context';
+/* 
+ Note: Change Background image depending on darkMode
+*/
 export const App: React.FC = () => {
   return (
     <UserProvider>
       <ThemeProvider>
         <NavigationContainer>
-          <Navigation />
+          <ImageBackground
+            style={{flex: 1, justifyContent: 'center'}}
+            source={require('../assets/images/background_dark.png')}>
+            <SafeAreaView style={{flex: 1}}>
+              <Navigation />
+            </SafeAreaView>
+          </ImageBackground>
         </NavigationContainer>
       </ThemeProvider>
     </UserProvider>
