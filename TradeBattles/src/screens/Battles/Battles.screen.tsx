fc4d0f74ea72a803dca86d7f5891817a3a87bd07
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-18 12:48:14 +0200

    structuring imports in battles screens

--- TradeBattles/src/screens/Battles/Battles.screen.tsx
+++ TradeBattles/src/screens/Battles/Battles.screen.tsx
@@ -1,12 +1,20 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, SafeAreaView, Pressable} from 'react-native';
-import type {Battle} from '../../shared/Types';
+
+/* ---- NAVIGATION ---- */
 import {useNavigation} from '@react-navigation/native';
 import {ProfileScreenNavigationProp} from '../../shared/Types';
-import LottieView from 'lottie-react-native';
+
+/* ---- COMPONENTS ---- */
+import {View, Text, SafeAreaView, Pressable} from 'react-native';
 import {BattleCardList} from '../../components/BattleCardList.component';
+import type {Battle} from '../../shared/Types';
+
+/* ---- CONTEXT ---- */
 import {useTheme} from '../../Contexts/Theme';
 
+/* ---- STYLES ---- */
+import {styles} from './Battles.styles';
+import LottieView from 'lottie-react-native';
 const pointingArrowSrc = require('../../../assets/lotties/pointing_arrow.json');
 
 export const Battles: React.FC = () => {
@@ -70,53 +78,5 @@ export const Battles: React.FC = () => {
     </SafeAreaView>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  header: {
-    fontSize: 30,
-    fontWeight: '800',
-    marginTop: 20,
-    marginBottom: -25,
-  },
-  createBattleButton: {
-    width: 40,
-    height: 40,
-    marginLeft: 'auto',
-    marginRight: 40,
-    marginTop: 20,
-    borderRadius: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: 'black',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowRadius: 3,
-    shadowOpacity: 0.2,
-  },
-  createBattleButtonText: {
-    fontSize: 30,
-    fontWeight: '700',
-    alignSelf: 'center',
-  },
-  no_battle_point_arrows: {
-    transform: [{rotate: '180deg'}],
-    paddingHorizontal: 60,
-    marginLeft: 'auto',
-    width: 70,
-    height: 70,
-  },
-  noBattlesMessage: {
-    fontSize: 30,
-    fontWeight: '300',
-    textAlign: 'center',
-    marginTop: '40%',
-    paddingHorizontal: 30,
-  },
-});
 
 // props to https://www.youtube.com/watch?v=hD5Hi_XG4lc for the carrousell animation
