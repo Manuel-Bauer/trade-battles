d40306115791a0d3ca67ef1109a1655443f890bd
Samuel Fanhais <samuelfanhais@gmail.com>
2022-06-18 11:05:12 +0200

    prisma query getAllUsers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const router = require("./router.js");
+const router = require('./router.js');
 const PORT = 3000;
+const { getAllUsers } = require('./models/users.model');
 
 app.use(express.json());
 app.use(router);
 
+getAllUsers().then((res) => console.log(res));
+
 app.listen(PORT, () => {
-	console.log(`We are in boys... http://localhost:${PORT}`);
+  console.log(`We are in boys... http://localhost:${PORT}`);
 });
