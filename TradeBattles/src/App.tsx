e0ae7909ce4f610c6387b9a267968c217f3e6d3f
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-18 09:20:19 +0200

    started refactor

--- TradeBattles/src/App.tsx
+++ TradeBattles/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+
+/* ---- NAVIGATION ---- */
 import {NavigationContainer} from '@react-navigation/native';
 import {Navigation} from './screens/LoginStackNavigator.navigation';
+
+/* ---- CONTEXT ---- */
 import {UserProvider, ThemeProvider} from './App.provider';
-import {LogBox} from 'react-native';
-LogBox.ignoreLogs(['Warning: ...']); // Ignore log notification by message
-LogBox.ignoreAllLogs(); //Ignore all log notifications
-import {showNotification} from './shared/Notification';
 
 export const App: React.FC = () => {
   return (
