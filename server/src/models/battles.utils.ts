3609a9ddac6bbf6a708bbb7cfc2aca09a65c2910
Maxim Buz <mbuz.maxim@gmail.com>
2022-06-23 10:17:47 +0200

    refactored stocks to array and added more stats

--- server/src/models/battles.utils.ts
+++ server/src/models/battles.utils.ts
@@ -1,72 +1,77 @@
 //@ts-nocheck
-import { getQuote } from "./quotes.model";
+import { getQuote } from './quotes.model';
 var memoize = require('underscore/cjs/memoize.js');
 
 export const groupBy = memoize(function (array, key) {
   if (!array) return null;
   return array.reduce((result, currentValue) => {
-    (result[currentValue[key]] = result[currentValue[key]] || []).push(
-      currentValue
-    );
+    (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
     return result;
   }, {});
 });
-
 
 export const calculateAverage = memoize(function (values) {
   return values.reduce((m, x, i) => m + (x - m) / (i + 1), 0);
 });
 
-
 export const calculateTotalBought = memoize(function (transactions) {
   return transactions.reduce((prev, _, index, arr) => {
-    if (arr[index].action == "BUY") return prev + (arr[index].price * arr[index].quantity);
+    if (arr[index].action == 'BUY') return prev + arr[index].price * arr[index].quantity;
     else return prev;
   }, 0);
 });
-
 
 export const calculateTotalSold = memoize(function (transactions) {
   return transactions.reduce((prev, _, index, arr) => {
-    if (arr[index].action == "SELL") return prev + (arr[index].price * arr[index].quantity);
+    if (arr[index].action == 'SELL') return prev + arr[index].price * arr[index].quantity;
     else return prev;
-  }, 0);
-}
-);
-
-
-export const calculateQuantity = memoize(function (transactions) {
-  return transactions.reduce((prev, _, index, arr) => {
-    if (arr[index].action == "BUY") return prev + (arr[index].quantity);
-    if (arr[index].action == "SELL") return prev - (arr[index].quantity);
   }, 0);
 });
 
+export const calculateQuantity = memoize(function (transactions) {
+  return transactions.reduce((prev, _, index, arr) => {
+    if (arr[index].action == 'BUY') return prev + arr[index].quantity;
+    if (arr[index].action == 'SELL') return prev - arr[index].quantity;
+  }, 0);
+});
 
-export function calculateStockStats (transactionsPerStock, currentPrices) {
+export function calculateStockStats(transactionsPerStock, currentPrices) {
   if (!transactionsPerStock) return null;
-  const result = {};
+  const result = [];
   Object.entries(transactionsPerStock).map((transaction) => {
-    const [key, value] = transaction;
+    const [symbol, value] = transaction;
     const totalBought = calculateTotalBought(value);
     const totalSold = calculateTotalSold(value);
     const owning = calculateQuantity(value);
-    const quote = currentPrices[value[0].symbol];
 
-    result[key] = {
+    const quote = currentPrices[value[0].symbol];
+    const currentPrice = quote.latestPrice;
+    const change = quote.change;
+    const changePercent = quote.changePercent;
+    const ytdChange = quote.ytdChange;
+
+    const totalValue = currentPrice * owning;
+    const averageBuyIn = calculateAverage(
+      value.filter((transaction) => transaction.action == 'BUY').map((transaction) => transaction.price)
+    );
+
+    result.push({
+      symbol,
       owning,
-      currentPrice: quote,
-      totalValue: quote * owning,
+      currentPrice,
+      change,
+      changePercent,
+      ytdChange,
+      averageBuyIn,
       totalBought,
       totalSold,
-      //@ts-ignore
-      averageBuyIn: calculateAverage(value.filter(transaction => transaction.action == "BUY").map(transaction => transaction.price))
-    };
+      totalValue,
+    });
   });
   return result;
-};
+}
 
-export function calculatePortfolioStats (startBudget, stockTransactions) {
+export function calculatePortfolioStats(startBudget, stockTransactions) {
   let remainingBudget = startBudget;
   let portfolioValue = null;
   if (stockTransactions) {
@@ -81,15 +86,17 @@ export function calculatePortfolioStats (startBudget, stockTransactions) {
     }
   }
   return { remainingBudget, portfolioValue };
-};
+}
 
-export async function getCurrentPrices (tickers) {
+export async function getCurrentPrices(tickers) {
   try {
     const stockPrices = {};
-    await Promise.all(tickers.map(async (ticker) => {
-      const quote = (await getQuote(ticker)).latestPrice;
-      stockPrices[ticker] = quote;
-    }));
+    await Promise.all(
+      tickers.map(async (ticker) => {
+        const quote = await getQuote(ticker);
+        stockPrices[ticker] = quote;
+      })
+    );
 
     return stockPrices;
   } catch (err) {
@@ -99,15 +106,15 @@ export async function getCurrentPrices (tickers) {
 
 export const getUsersFromBattles = memoize(function (battles) {
   const users = {};
-  battles.forEach(battle => {
-    battle.users.forEach(user => { users[user.id] = user; });
+  battles.forEach((battle) => {
+    battle.users.forEach((user) => {
+      users[user.id] = user;
+    });
   });
   return users;
 });
 
-
 export const getTickersFromBattles = memoize(function (battles) {
   const allTransactions = battles.reduce((prev, curr) => [...prev, ...curr.transaction], []);
-  return [... new Set(allTransactions.map(transaction => transaction.symbol))];
+  return [...new Set(allTransactions.map((transaction) => transaction.symbol))];
 });
-
--- server/src/router.ts
+++ server/src/router.ts
@@ -1,7 +1,7 @@
-import { postBattle, getMyBattles, updateBattle } from "./controllers/battles.controller";
-import { getHistoricalData, getQuote } from "./controllers/quotes.controller";
-import { postTransaction } from "./controllers/transactions.controller";
-import { getAllUsers, getUser, patchWatchlist, postUser } from "./controllers/users.controller";
+import { postBattle, getMyBattles, updateBattle } from './controllers/battles.controller';
+import { getHistoricalData, getQuote } from './controllers/quotes.controller';
+import { postTransaction } from './controllers/transactions.controller';
+import { getAllUsers, getUser, patchWatchlist, postUser } from './controllers/users.controller';
 
 const router = require('express').Router();
 
@@ -15,7 +15,7 @@ router.patch('/battles/:battleId', updateBattle);
 
 // Users
 router.get('/users', getAllUsers);
-router.get('/users/:id',getUser);
+router.get('/users/:id', getUser);
 router.post('/users', postUser);
 router.patch('/users/watchlist/:userId', patchWatchlist);
 
